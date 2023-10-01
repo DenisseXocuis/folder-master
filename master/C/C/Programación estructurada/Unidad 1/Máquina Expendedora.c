41cdb4bfa81c00a944a7ca92048787cbdb66bcf5
DenisseXocuis <denissexocuis@gmail.com>
2023-10-01 00:10:32 -0600

    changes

--- master/C/C/Programación estructurada/Unidad 1/Máquina Expendedora.c
+++ master/C/C/Programación estructurada/Unidad 1/Máquina Expendedora.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define T 2 //10, 5, 2 y 1|
+#define T 3 //10, 5, 2 y 1|
 void menu();
 void pause();
+void help();
+void error();
 /*dar de alta un producto y después agregar productos 
 programa interactivo, busqueda de productos, insercion de productos, productos.
 
@@ -16,7 +18,7 @@ typedef struct{
 }datos;
 
 int main(){
-    int i,op,p=0;
+    int i,op,p=0, clave2;
     datos productos[T];
     
     do{
@@ -28,7 +30,7 @@ int main(){
                 fflush(stdin);
                 puts("--------Alta de productos--------");
                 putchar('\n');
-                for(int i = 0, op2 = 0; i<T; i++){
+                for(int i = 0, op2 = 1; i<T; i++){
                 system("clear||cls");
                 puts("Ingresa la clave del producto");
                 scanf("%d", &productos[i].clave);
@@ -41,15 +43,14 @@ int main(){
                 scanf("%f", &productos[i].precio);
                 p++; //pivote
                 if((p+1)<=T){
-                    puts("¿Desea agregar otro producto? (1:SI, 2:NO)");
-                    scanf("%d", &op2);
+                    while(op2){
+                        puts("¿Desea agregar otro producto? (1:SI, 2:NO)");
+                        scanf("%d", &op2);
+                        if(op2==1 || op2==2)
+                            break;
+                    }
                     if(op2==2)
                         break;
-                    else if(op2!=1){
-                        puts("ERROR: Introdujo una opcion invalida, se regresara al menu de opciones");
-                        pause();
-                        break;
-                    }
                 }
                 else{
                     puts("Ya no hay espacio disponible para otro articulo, se regresara al menu de opciones");
@@ -57,29 +58,47 @@ int main(){
                 }
                 }
             break;
-            case 2:
+            case 2: system("clear||cls");
+                    printf("Ingresa la clave del producto\n>");
+                    scanf("%d", &clave2);
+                    puts("----------Mostrando todos los productos------------");
+                    for(int i = 0; i<(p+1); i++){
+                    if(productos[i].clave == clave2){
+                    printf("Nombre del producto: \t\t%s\n", productos[i].nombre);
+                    printf("Cantidad \t\t\t%d\n", productos[i].cantidad);
+                    printf("Precio  \t\t\t$%g\n", productos[i].precio);
+                    puts("---------------------------------------------");
+                    break;
+                    }
+                    }
+                    pause();
             break;
             case 3:
             break;
             case 4:
                 system("clear||cls");
-                puts("----------Mostrando todos los productos------------");
+                puts("----------Mostrando información del producto------------");
                 putchar('\n');
              for(int i = 0; i<p; i++){
-                printf("Nombre del producto: \t%s\n", productos[i].nombre);
-                printf("Clave: \t\t\t%d\n", productos[i].clave);
+                printf("Nombre del producto: \t\t%s\n", productos[i].nombre);
+                printf("Clave: \t\t\t\t%d\n", productos[i].clave);
                 printf("Cantidad \t\t\t%d\n", productos[i].cantidad);
-                printf("Precio \t\t\t%d\n", productos[i].precio);
+                printf("Precio  \t\t\t$%g\n", productos[i].precio);
                 puts("---------------------------------------------");
             }
             pause();
             break;
             case 5:
             break;
+            case 6:
+                    system("clear||cls");
+                    help();
+                    pause();
+            break;
             case 0: putchar('\n');
                     puts("Apagando maquina....");
             break;
-            default:
+            default:    error();
 
         }
 
@@ -112,10 +131,10 @@ void menu(){
     putchar('\n');
     puts("[1] Alta de productos (inventario)");
     puts("[2] Consulta de un producto");
-    puts("[3] Actualizar productos"); //cantidad
+    puts("[3] Actualizar productos"); //cantidad /////////////////////7
     puts("[4] Consulta de todos los productos");
-    puts("[5] Comprar productos");
-    puts("[6] Ayuda");
+    puts("[5] Comprar productos"); ///////////////////////77
+    puts("[6] Acerca de");
     puts("[0] Apagar");
     printf("Opcion \n>");
 
@@ -125,4 +144,27 @@ void pause(){
     puts("Pulse cualquier tecla para salir....");
     getchar();
     getchar();
+}
+void error(){
+    puts("ERROR: Opcion no valida");
+    puts("Pulse cualquier tecla para volver a intentar...");
+    getchar();
+    getchar();
+}
+void help(){
+    puts("------------------------Acerca de------------------------");
+    putchar('\n');
+    puts("[1] - Agregar productos de acuerdo al espacio de la maquina");
+    puts("[2] - Mostrar la informacion de un producto en especifico de acuerdo a su clave");
+    puts("[3] - Actualizar la cantidad de productos");
+    puts("[4] - Mostrar todos los productos del inventario");
+    puts("[5] - Comprar los productos");
+    putchar('\n');
+    puts("---------------------------------------------------------");
+    puts("\t         Maquina Expendedora v0.3");
+    puts("\t    by Ing. Lara Xocuis Martha Denisse");
+    puts("---------------------------------------------------------");
+    puts("Ingeniería Informática S22 - 1ro de Octubre del 2023");
+    putchar('\n');
+    
 }
