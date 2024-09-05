9524f81682548feb6976ea2d788e77176e2eeefe
DenisseXocuis <denissexocuis@gmail.com>
2024-09-04 20:35:15 -0600

    change

---
+++ master/java/Proyectito1.java
@@ -0,0 +1,14 @@
+/*My first hello world in java*/
+
+package proyectito1;
+
+/**
+ * @author denisso
+ */
+public class Proyectito1 {
+    public static void main(String[] args) {
+       System.out.println("Bonjour a tout le monde!!");
+    }
+}
+
+
